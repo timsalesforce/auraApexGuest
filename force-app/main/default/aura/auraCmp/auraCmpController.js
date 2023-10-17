1b145bd082fb0dd7848a00dd2f0ba4bc553b58ab
Tim Dionne <tdionne@salesforce.com>
2023-10-17 09:48:05 -0700

    init

---
+++ force-app/main/default/aura/auraCmp/auraCmpController.js
@@ -0,0 +1,36 @@
+({
+    doInit : function(cmp, _event, _helper) {
+        // create a one-time use instance of the serverEcho action
+        // in the server-side controller
+        var action = cmp.get("c.getCustomObjects");
+
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                // Alert the user with the value returned 
+                // from the server
+                cmp.set('v.objs', response);
+
+                // You would typically fire a event here to trigger 
+                // client-side notification that the server-side 
+                // action is complete
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
+    }
+})
